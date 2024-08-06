1abd0383c3fb14da57ac6ff0f72fb5aabc653bdc
Mateus Felipe <brasil.mateus748@gmail.com>
2024-08-05 22:54:36 -0300

    Add grid items div inside grid-container div

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/CardGrid.js';
+import './components/DataFetcher.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+      <div className="grid-item"><img src={} alt="" /></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
+      <div className="grid-item"></div>
     </div>
   );
 }
